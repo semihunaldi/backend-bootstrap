a62ff3bffb6df6bbc67ca7024a9acdc64272acb3
semihunaldi <semihunaldi@gmail.com>
2018-11-21 11:02:22 +0300

    fix

---
+++ ws/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO t_user (id,deleted,age,email,first_name,last_name,location,mobile_phone,user_name) VALUES ('9B18591A-C518-4CDE-BA3B-9ADC3145D677','0','25','test@test.com','FirstTest','LastTest','TR','5331234565','test');
+commit;
