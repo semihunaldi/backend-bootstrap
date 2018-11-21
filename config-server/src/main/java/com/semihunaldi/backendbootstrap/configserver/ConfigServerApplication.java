c34fec7f8a4a328d093cdf9f37cdc0275b39597d
semihunaldi <semihunaldi@gmail.com>
2018-11-21 22:12:12 +0300

    config server implemented

---
+++ config-server/src/main/java/com/semihunaldi/backendbootstrap/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.semihunaldi.backendbootstrap.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by semihunaldi on 21.11.2018
+ */
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
---
+++ config-server/src/main/java/com/semihunaldi/backendbootstrap/configserver/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.semihunaldi.backendbootstrap.configserver.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by semihunaldi on 21.11.2018
+ */
+
+@Configuration
+@EnableConfigServer
+public class AppConfig {
+
+}
