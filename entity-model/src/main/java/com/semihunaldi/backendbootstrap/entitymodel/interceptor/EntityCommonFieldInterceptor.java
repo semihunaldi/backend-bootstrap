6bb59bf59cdcf4b9e3fa47d8541d561e1ba617e5
semihunaldi <semihunaldi@gmail.com>
2018-11-29 11:51:37 +0300

    entity interceptor configured properly, but will moved to services module

--- entity-model/src/main/java/com/semihunaldi/backendbootstrap/entitymodel/interceptor/EntityCommonFieldInterceptor.java
+++ entity-model/src/main/java/com/semihunaldi/backendbootstrap/entitymodel/interceptor/EntityCommonFieldInterceptor.java
@@ -7,10 +7,11 @@ import org.hibernate.EmptyInterceptor;
 import org.hibernate.type.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -19,74 +20,67 @@ import java.util.Date;
  */
 
 @Component
-public class EntityCommonFieldInterceptor extends EmptyInterceptor
-{
+public class EntityCommonFieldInterceptor extends EmptyInterceptor {
+
     private final static Logger log = LoggerFactory.getLogger(EntityCommonFieldInterceptor.class);
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    private Optional<MethodCallInformation> findMethodCallParameter()
-    {
-        //TODO: session
-//        MethodCallInformation = (MethodCallInformation) applicationContext.getBean("userSessionInformation");
+    private Optional<MethodCallInformation> findMethodCallParameter() {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         MethodCallInformation methodCallInformation = MethodCallInformation.instanceOf();
+        if(requestAttributes != null){
+            HttpServletRequest request = requestAttributes.getRequest();
+            if(request != null){
+                if(request.getUserPrincipal() != null){
+                    String userName = request.getUserPrincipal().getName();
+                    methodCallInformation.setUsername(userName);
+                }
+                String remoteAddr = request.getRemoteAddr();
+                methodCallInformation.setIp(remoteAddr);
+            }
+        }
         return Optional.of(methodCallInformation);
     }
 
     @Override
-    public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types)
-    {
+    public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
         updateCommonFieldsOnInsert(entity, state, propertyNames);
         return true;
     }
 
     @Override
-    public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types)
-    {
+    public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
         updateCommonFieldsOnUpdate(entity, currentState, propertyNames);
         return true;
     }
 
-    private void updateCommonFieldsOnInsert(Object p_entity, Object[] p_state, String[] p_propertyNames)
-    {
-        try
-        {
+    private void updateCommonFieldsOnInsert(Object p_entity, Object[] p_state, String[] p_propertyNames) {
+        try{
             Optional<MethodCallInformation> methodCallInformation = findMethodCallParameter();
-            if (methodCallInformation.isPresent() && p_entity instanceof AbstractEntity)
-            {
+            if(methodCallInformation.isPresent() && p_entity instanceof AbstractEntity){
                 AbstractEntity abstractEntity = (AbstractEntity) p_entity;
                 setCreateInfo(abstractEntity, p_state, p_propertyNames, methodCallInformation);
                 setUpdateInfo(abstractEntity, p_state, p_propertyNames, methodCallInformation);
             }
-        }
-        catch (Exception e)
-        {
+        } catch(Exception e){
             log.error("Failed to set common fields.", e);
             throw new RuntimeException(e);
         }
     }
 
-    private void updateCommonFieldsOnUpdate(Object p_entity, Object[] p_state, String[] p_propertyNames)
-    {
-        try
-        {
+    private void updateCommonFieldsOnUpdate(Object p_entity, Object[] p_state, String[] p_propertyNames) {
+        try{
             Optional<MethodCallInformation> methodCallInformation = findMethodCallParameter();
-            if (methodCallInformation.isPresent() && p_entity instanceof AbstractEntity)
-            {
+            if(methodCallInformation.isPresent() && p_entity instanceof AbstractEntity){
                 AbstractEntity abstractEntity = (AbstractEntity) p_entity;
                 setUpdateInfo(abstractEntity, p_state, p_propertyNames, methodCallInformation);
             }
-        }
-        catch (Exception e)
-        {
+        } catch(Exception e){
             log.error("Failed to set common fields.", e);
             throw new RuntimeException(e);
         }
     }
 
-    private void setUpdateInfo(AbstractEntity abstractEntity, Object[] p_state, String[] p_propertyNames, Optional<MethodCallInformation> methodCallInformation)
-    {
+    private void setUpdateInfo(AbstractEntity abstractEntity, Object[] p_state, String[] p_propertyNames, Optional<MethodCallInformation> methodCallInformation) {
         abstractEntity.setUpdateTime(new Date());
         setPropertyState(p_state, p_propertyNames, "updateTime", abstractEntity.getUpdateTime());
 
@@ -97,30 +91,24 @@ public class EntityCommonFieldInterceptor extends EmptyInterceptor
         setPropertyState(p_state, p_propertyNames, "updateUserIP", abstractEntity.getUpdateUserIP());
     }
 
-    private void setCreateInfo(AbstractEntity abstractEntity, Object[] p_state, String[] p_propertyNames, Optional<MethodCallInformation> methodCallInformation)
-    {
+    private void setCreateInfo(AbstractEntity abstractEntity, Object[] p_state, String[] p_propertyNames, Optional<MethodCallInformation> methodCallInformation) {
         abstractEntity.setCreateTime(new Date());
         setPropertyState(p_state, p_propertyNames, "createTime", abstractEntity.getCreateTime());
 
-        if (StringUtils.isBlank(abstractEntity.getCreateUser()))
-        {
+        if(StringUtils.isBlank(abstractEntity.getCreateUser())){
             abstractEntity.setCreateUser(methodCallInformation.get().getUsername());
             setPropertyState(p_state, p_propertyNames, "createUser", abstractEntity.getCreateUser());
         }
 
-        if (StringUtils.isBlank(abstractEntity.getCreateUserIP()))
-        {
+        if(StringUtils.isBlank(abstractEntity.getCreateUserIP())){
             abstractEntity.setCreateUserIP(methodCallInformation.get().getIp());
             setPropertyState(p_state, p_propertyNames, "createUserIP", abstractEntity.getCreateUserIP());
         }
     }
 
-    private void setPropertyState(Object[] propertyStates, String[] propertyNames, String propertyName, Object propertyState)
-    {
-        for (int i = 0; i < propertyNames.length; i++)
-        {
-            if (propertyName.equals(propertyNames[i]))
-            {
+    private void setPropertyState(Object[] propertyStates, String[] propertyNames, String propertyName, Object propertyState) {
+        for(int i = 0; i < propertyNames.length; i++){
+            if(propertyName.equals(propertyNames[i])){
                 propertyStates[i] = propertyState;
                 return;
             }
