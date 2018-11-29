34e13331b104622ce84370a777dae697293627e6
semihunaldi <semihunaldi@gmail.com>
2018-11-29 13:44:43 +0300

    spring warn logs fixed

--- auth-server/src/main/java/com/semihunaldi/backendbootstrap/authserver/config/AppConfig.java
+++ auth-server/src/main/java/com/semihunaldi/backendbootstrap/authserver/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.semihunaldi.backendbootstrap.authserver.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableEurekaClient
-@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class, MongoRepositoriesAutoConfiguration.class})
 public class AppConfig {
 
 }
