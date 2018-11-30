24101ccd2c22f64a358c45b9eaea77e5b93e9a18
semihunaldi <semihunaldi@gmail.com>
2018-11-30 13:35:32 +0300

    in memory mongo test impl

---
+++ ws/src/test/java/com/semihunaldi/backendbootstrap/ws/MongoInMemoryTest.java
@@ -0,0 +1,58 @@
+package com.semihunaldi.backendbootstrap.ws;
+
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by semihunaldi on 30.11.2018
+ */
+
+public class MongoInMemoryTest {
+
+	private MongodExecutable mongodExecutable;
+	private MongoTemplate mongoTemplate;
+
+	private static final String ip = "localhost";
+	private static final int port = 27017;
+
+	@After
+	public void clean() {
+		mongodExecutable.stop();
+	}
+
+	@Before
+	public void init() throws Exception {
+
+		IMongodConfig mongodConfig = new MongodConfigBuilder().version(Version.Main.PRODUCTION)
+				.net(new Net(ip, port, Network.localhostIsIPv6()))
+				.build();
+
+		MongodStarter starter = MongodStarter.getDefaultInstance();
+		mongodExecutable = starter.prepare(mongodConfig);
+		mongodExecutable.start();
+		mongoTemplate = new MongoTemplate(new MongoClient(ip, port), "test");
+	}
+
+	@Test
+	public void test() {
+		DBObject objectToSave = BasicDBObjectBuilder.start()
+				.add("key", "value")
+				.get();
+		mongoTemplate.save(objectToSave, "collection");
+		assertThat(mongoTemplate.findAll(DBObject.class, "collection")).extracting("key").containsOnly("value");
+	}
+}
